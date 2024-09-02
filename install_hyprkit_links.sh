a8b87d09493fa2e2cb8ab7792388a9b276f4a440
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2024-09-01 23:54:14 -0700

    fix hashbangs

---
+++ install_hyprkit_links.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+INSTALL_PATH=$1
+
+if [ -z "$INSTALL_PATH" ]; then
+    echo "usage: $0 <install/path>"
+    exit 1
+fi
+
+[ -e "$INSTALL_PATH/bedtime.sh" ] || ln -s "$PWD/bedtime.sh"  "$INSTALL_PATH/bedtime.sh"
+[ -e "$INSTALL_PATH/volume.sh" ] || ln -s "$PWD/volume.sh"  "$INSTALL_PATH/volume.sh"
+[ -e "$INSTALL_PATH/notify.sh" ] || ln -s "$PWD/notify.sh"  "$INSTALL_PATH/notify.sh"
+[ -e "$INSTALL_PATH/encrypt" ] || ln -s "$PWD/encrypt"  "$INSTALL_PATH/encrypt"
+[ -e "$INSTALL_PATH/crypto-note" ] || ln -s "$PWD/crypto-note"  "$INSTALL_PATH/crypto-note"
+[ -e "$INSTALL_PATH/grimsel" ] || ln -s "$PWD/grimsel"  "$INSTALL_PATH/grimsel"
