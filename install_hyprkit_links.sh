6dc0d49de746918508a08d005f911b5b6200e571
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2024-09-06 02:27:08 -0700

    fix swayfocus script

--- install_hyprkit_links.sh
+++ install_hyprkit_links.sh
@@ -13,3 +13,5 @@ fi
 [ -e "$INSTALL_PATH/encrypt" ] || ln -s "$PWD/encrypt"  "$INSTALL_PATH/encrypt"
 [ -e "$INSTALL_PATH/crypto-note" ] || ln -s "$PWD/crypto-note"  "$INSTALL_PATH/crypto-note"
 [ -e "$INSTALL_PATH/grimsel" ] || ln -s "$PWD/grimsel"  "$INSTALL_PATH/grimsel"
+[ -e "$INSTALL_PATH/tmenu" ] || ln -s "$PWD/tmenu"  "$INSTALL_PATH/tmenu"
+[ -e "$INSTALL_PATH/dm-swayfocus" ] || ln -s "$PWD/dm-swayfocus"  "$INSTALL_PATH/dm-swayfocus"
