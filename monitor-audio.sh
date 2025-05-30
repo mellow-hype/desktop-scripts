8cc47b8c2e6fc962f4213de800c00809b6a34143
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2025-05-30 16:26:46 -0700

    quotes

--- monitor-audio.sh
+++ monitor-audio.sh
@@ -2,7 +2,7 @@
 # Kill any instances of the script which are already running
 PREV_PID=$(ps -L -Ao ppid,pid,cmd | grep -E "sleep.(2|5)" | grep -v "grep" | cut -d ' ' -f3)
 if [ ! -z "${PREV_PID}" ]; then
-    echo ${PREV_PID} | xargs kill -9
+    echo "${PREV_PID}" | xargs kill -9
 fi
 
 while true; do
