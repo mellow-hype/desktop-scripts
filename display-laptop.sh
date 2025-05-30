803cc5979bb2f970e0cf7893478893bb465d76cf
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2025-05-29 23:30:16 -0700

    remove legacy scripts

--- display-laptop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-intern="eDP-1"
-extern="DP-3"
-xrandr --output "$extern" --off --output "$intern" --auto
--- display-ultrawide.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-intern="eDP-1"
-extern="DP-3"
-extern_res="5120x1440"
-xrandr --output "$intern" --off --output "$extern" --mode "$extern_res"
